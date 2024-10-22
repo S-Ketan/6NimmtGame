df984ce613b3b767d70fd9c4a1c07ce9b2a18e6d
Ketan Sharma <ketan10923@gmail.com>
2024-10-22 12:45:44 +0200

    initial commit

---
+++ Card.java
@@ -0,0 +1,42 @@
+package SechzNimmt;
+
+public class Card {
+
+// Internal state
+    private int number;
+    private int hornOchs = 1; // Initialize properly
+
+    // Constructor for the Card class (no return type)
+    public Card(int nr) {
+        number = nr;
+    }
+
+    // Dynamic behavior
+    public int getNumber() {
+        return number;
+    }
+
+    public int getOchsPoints() {
+        // Reinitialize hornOchs based on card number logic
+        if (number == 55) {
+            hornOchs = 7;
+        } else if (number % 10 == 5) {
+            hornOchs = 2;
+        } else if (number % 10 == 0) {
+            hornOchs = 3;
+        } else if (number % 11 == 0) {
+            hornOchs = 5;
+        } else {
+            hornOchs = 1; // Default case
+        }
+
+        return hornOchs;
+    }
+
+    public void printInformation() {
+        System.out.println("Number of card: " + number);
+        System.out.println("Horn Ochs points: " + getOchsPoints());
+    }
+}
+
+  
---
+++ main.java
@@ -0,0 +1,14 @@
+package SechzNimmt;
+
+
+public class main {
+    public static void main(String[] args) {
+        Card card1 = new Card(42);
+        card1.printInformation();
+        
+
+        Card card2 = new Card(55);
+        card2.printInformation();
+        
+    }
+}
